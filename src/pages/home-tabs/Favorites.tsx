75c30d15eb3ea61a1b59273010657ee9fd3abfdd
angelqtypie <angeljustineballaso07@gmail.com>
2025-04-16 16:00:37 +0800

    Updated hoe-tabs styles

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -15,10 +15,15 @@ function Favorites() {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-
-                  <IonTitle style={{ textAlign: 'center' }}>Capybara Mood</IonTitle>
-
+        <IonToolbar color="dark">
+<div style={{ textAlign: 'center', padding: '10px 0' }}>
+             <IonTitle style={{ fontWeight: 'bold', fontSize: '22px', color: 'var(--ion-color-light)' }}>
+               Capybara Mood
+             </IonTitle>
+             <p style={{ margin: 0, fontSize: '14px', color: 'var(--ion-color-light-tint)' }}>
+               Pick your moods with a chill capybara
+             </p>
+           </div> 
           <IonButtons slot='start'>
           <IonMenuButton />
           </IonButtons>
--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -14,11 +14,18 @@ const Search: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar style={{ padding: '0' }}> {/* Remove any padding to reduce space */}
+        <IonToolbar color="dark"> 
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
-          <IonTitle style={{ textAlign: 'center', fontWeight: 'bold' }}>Capybara Mood</IonTitle>
+           <div style={{ textAlign: 'center', padding: '10px 0' }}>
+             <IonTitle style={{ fontWeight: 'bold', fontSize: '22px', color: 'var(--ion-color-light)' }}>
+               Capybara Mood
+             </IonTitle>
+             <p style={{ margin: 0, fontSize: '14px', color: 'var(--ion-color-light-tint)' }}>
+               Search your moods with a chill capybara
+             </p>
+           </div> 
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
